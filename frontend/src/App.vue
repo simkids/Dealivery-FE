0c67f04e00fb22ef51cd331b0fae518958fdc76a
syy0O <songie05@naver.com>
2024-11-18 14:55:34 +0900

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+/* global IMP */
+let isImpInitialized = false;
+
+export default {
+  name: "App",
+  components: {},
+  created() {
+    if (!isImpInitialized) {
+      IMP.init(process.env.VUE_APP_PORTONE_STORE_ID);
+      isImpInitialized = true;
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+}
+</style>
